2865ff96e74d4664b1e0eefaaf2523e23dd16434
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-03-03 23:33:28 +0200

    Integrate routes into the application

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import routes from './routes.js';
 
 const app = express();
 
@@ -18,12 +19,8 @@ try {
 //     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
 //     next();
 // });
+app.use(express.json());
 app.use(cors());
+app.use(routes);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'it works!' });
-});
-app.get('/data/catalog', (req, res) => {
-    res.json({ message: 'Some data' });
-});
 app.listen(3030, () => console.log('RESTful server os running on http://localhost:3030...'));
