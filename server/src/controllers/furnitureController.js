590d0016286ee7256984c21a810e73181314a869
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-03-07 18:34:00 +0200

    Add update functionality for furniture items

--- server/src/controllers/furnitureController.js
+++ server/src/controllers/furnitureController.js
@@ -12,6 +12,15 @@ furnitureController.post('/', isAuth, async (req, res) => {
 
     res.json(furniture);
 });
+
+//update
+furnitureController.put('/:furnitureId', async (req, res) => {
+    const furnitureData = req.body;
+    const furnitureId = req.params.furnitureId;
+    const newFurntiture = await furnitureService.update(furnitureId, furnitureData);
+    res.json(newFurntiture);
+});
+
 //delete
 furnitureController.delete('/:furnitureId', async (req, res) => {
     await furnitureService.delete(req.params.furnitureId);
